81680174c657cfb3a8ecdab86fe8d3e7c438c40d
mac <macbook@MacBook-Pro-de-mac.local>
2013-10-10 08:10:39 -0500

    primer commit de Refinery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Platensa::Application.config.secret_token = '313ae77bc26dfd8c5be5662d2bcd877ab08c86f108b516eed45fc191f2d59bca24a136ef000d47b34f9ad0abdf2e71003ada7eba9fce591fb622cb6167fa1d0c'
